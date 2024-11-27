cb497f7d9a3921a2a347c4dbb3fbf779c5c5e693
Bethina S.M <bethina.s.moen@gmail.com>
2024-11-27 11:22:45 +0100

    Finished (?)

---
+++ krokodille/krokodille/Krokodille.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace krokodilleOppgave
+{
+    internal class Krokodille
+    {
+        public void Run()
+        {
+            Random rand = new Random();
+
+            int score = 0;
+            bool isPlaying = true;
+
+            while (isPlaying)
+            {
+                int randomNr1 = rand.Next(0, 11); // inside loop so they always generate new numbers
+                int randomNr2 = rand.Next(0, 11);
+
+                Console.WriteLine($"{randomNr1} _ {randomNr2}");
+                var userInput = Console.ReadLine();
+
+                if (randomNr1 < randomNr2 && userInput == "<" || randomNr1 > randomNr2 && userInput == ">" || randomNr1 == randomNr2 && userInput == "=")
+                {
+                    score++;
+                    Console.WriteLine($"Correct! You get a point! Your score is now: {score}");
+                }
+                else if(userInput == "<" || userInput == ">" || userInput == "=")
+                {
+                    score--;
+                    Console.WriteLine($"Wrong answer.. You lose a point. Your score is now: {score}");
+                }
+                else // if the symbol written does not match <, >, or =
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("Invalid answer. Game Over.");
+                    isPlaying = false; // exits loop
+                }
+
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine($"Your final score is: {score}");
+            Console.WriteLine("The game will now shut down.");
+
+            Thread.Sleep(3000);
+            Environment.Exit(0); 
+        }
+    }
+}
--- krokodille/krokodille/Program.cs
+++ krokodille/krokodille/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Threading.Channels;
+using krokodilleOppgave;
+
+Console.WriteLine("Oppgave: Krokodillespillet");
+ /* OPPGAVE:
+    Bruk det du har lært til å programmere “krokodille spillet”.
+    
+    For hver runde skal det printes ut til skjermen et random tall mellom 1-11, 
+    et mellomrom og et nytt tall mellom 1-11,
+    med en underscore mellom slik at det ser sånn ut: 3 _ 5
+    
+    Brukeren kan skrive inn <, > eller =
+    (så hvis brukeren får 3 _ 5 vil svaret være <, altså 3 < 5)
+    
+    Tallene må sjekkes om det første er større eller mindre eller 
+    lik det andre tallet,
+
+    Det må verifiseres om brukeren har valgt riktig alternativ.
+
+    Brukeren får et poeng per riktig svar, og mister et poeng per feil svar.
+
+    Poengsummen printes til skjermen for hvert svar brukeren har gitt.
+
+    Spillet avsluttes når brukeren skriver inn noe annet enn de tre alternativene.
+  */
+
+Console.WriteLine("Two random numbers will be printed. Which number is bigger? Or maybe they are equal?");
+Console.WriteLine("Use < , > or = ");
+Console.WriteLine();
+Console.WriteLine("If you enter any other symbol than these three, the game ends.");
+Console.WriteLine();
+Console.WriteLine("Right answer gives you a point, wrong answer removes a point from your score.");
+Console.WriteLine("Good luck!");
+Console.WriteLine();
+
+Thread.Sleep(2000);
+Console.WriteLine();
+
+var Krokodille = new Krokodille();
+Krokodille.Run();
